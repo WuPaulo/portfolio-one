0b3a100036ef00566031fd17784fa4e7224605e5
WuPaulo <paulowbusiness@gmail.com>
2021-04-26 22:43:32 -0400

    more styling

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ menuBtn.addEventListener("click", () => {
   mobile.classList.toggle("active");
   menuBtn.classList.toggle("open");
 });
+
+const sectionOneOptions = {
+  rootMargin: "-300px 0px 0px 0px",
+};
+
+const header = document.querySelector("nav");
+const sectionOne = document.querySelector(".introduction");
+
+const sectionOneObserver = new IntersectionObserver(function (
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      header.classList.add("navScrolled");
+    } else {
+      header.classList.remove("navScrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
