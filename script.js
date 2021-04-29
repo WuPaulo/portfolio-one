37bd81b6b98bd286b2986f98d59b86565bb85202
WuPaulo <paulowbusiness@gmail.com>
2021-04-29 17:10:23 -0400

    more

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ sectionOneOptions);
 sectionOneObserver.observe(sectionOne);
 
 const sectionTwoOptions = {
-  rootMargin: "0px 0px 0px 0px",
+  rootMargin: "10px 0px 0px 0px",
 };
 
 const sectionTwo = document.querySelector(".downArrow");
